ce0acb22af12ff9f4eb6666466eab040ea7a02df
saravananram9523 <saravananram9523@gmail.com>
2022-06-09 23:40:09 +0530

    solved

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -99,15 +99,15 @@ const mutationMethods = new GraphQLObjectType({
                 name : {type : GraphQLNonNull(GraphQLString)}, 
                 description : {type : GraphQLNonNull(GraphQLString)},
                 status : {
-                    type : new GraphQLEnumType({
-                    name : 'ProjectStatus',
-                    values: {
-                        new: {value: "not started"},
-                        progress : {value : 'in progress'},
-                        done : {value: "Completed"}
-                    }, 
-                }),
-                defaultValue : 'not started'
+                    type: new GraphQLEnumType({
+                        name: 'ProjectStatus',
+                        values: {
+                            new: { value: 'Not Started' },
+                            progress: { value: 'In Progress' },
+                            completed: { value: 'Completed' },
+                        },
+                    }),
+                defaultValue: 'Not Started',
                 },
             clientId : {type : GraphQLNonNull(GraphQLID)}
             }, 
